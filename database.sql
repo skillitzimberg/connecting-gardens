6b2e155679dfce87ca455b3d60c93638e3e9a912
Jessica Willis <jessica_willis_92@yahoo.com>
2019-01-12 15:19:42 -0800

    database script working

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+drop database growinggardens;
+
 CREATE DATABASE IF NOT EXISTS growinggardens DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE growinggardens;
 
@@ -16,20 +18,20 @@ CREATE TABLE user (
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
   category ENUM('CONSUMER', 'PRODUCER', 'BOTH'),
-  garden_type ENUM('IN_GROUND', 'ACCESSIBLE_RAISE', 'CONTAINERS', 'COMMUNITY_PLOT', 'LEAD_FREE'),
-)
+  garden_type ENUM('IN_GROUND', 'ACCESSIBLE_RAISE', 'CONTAINERS', 'COMMUNITY_PLOT', 'LEAD_FREE')
+);
 
 CREATE TABLE inventory (
   uid int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   category varchar(255) NOT NULL,
   name varchar(255) NOT NULL,
   description varchar(255) NOT NULL
-)
+);
 
 CREATE TABLE user_inventory (
   user_uid int(11) NOT NULL PRIMARY KEY,
   quantity varchar(255) NOT NULL
-)
+);
 
 CREATE TABLE message (
   message_uid int(11) NOT NULL PRIMARY KEY,
@@ -42,4 +44,4 @@ CREATE TABLE message (
   preferred_date1 DATETIME NOT NULL,
   preferred_date2 DATETIME NOT NULL,
   preferred_date3 DATETIME NOT NULL
-)
+);
